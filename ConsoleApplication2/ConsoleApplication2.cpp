8031a087fa2c2f6eff79a99e31887fd8166232d9
270287893 <270287893@CIT3-205-06>
2023-08-22 13:11:57 +1200

    Add project files.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,36 @@
+ 
+
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <array>
+#include <windows.h>
+
+using namespace std;
+
+
+
+
+
+
+int main()
+{
+   
+    vector<string, int> cities;
+    cities[0] = "Invercargill", 56800;
+   
+    string answer;
+
+ cout << "Which city?" << endl;
+ cin >> answer;
+
+    
+
+
+
+
+
+    system("pause>0");
+}
+
